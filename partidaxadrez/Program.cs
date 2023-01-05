34419b6325c8e1ec568dcecf14b2982b7d5f5c61
Felipe Augusto <felipe.acguedes@gmail.com>
2023-01-05 20:24:07 -0300

    Começando a implementar o tabuleiro

--- partidaxadrez/Program.cs
+++ partidaxadrez/Program.cs
@@ -7,12 +7,7 @@ namespace partidaxadrez
     {
         static void Main(string[] args)
         {
-            Posicao p;
-
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
-
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
---
+++ partidaxadrez/tabuleiro/Peca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.qteMovimentos = 0;
+        }
+    }
+}
