fe86293b9a1bb5ac45a6488aa64a802ec7209b13
Felipe Augusto <felipe.acguedes@gmail.com>
2023-01-05 20:04:18 -0300

    Projeto criado

---
+++ partidaxadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using partidaxadrez.tabuleiro;
+
+namespace partidaxadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+        }
+    }
+}
