4a562bf7f91dc077eee8986642a4633ec4a975f2
Felipe Augusto <felipe.acguedes@gmail.com>
2023-01-05 22:32:03 -0300

    Começando a implementar um método que imprime o tabuleiro

--- partidaxadrez/Program.cs
+++ partidaxadrez/Program.cs
@@ -8,6 +8,10 @@ namespace partidaxadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
--- partidaxadrez/tabuleiro/Tabuleiro.cs
+++ partidaxadrez/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     class Tabuleiro
     {
@@ -17,6 +11,11 @@ namespace tabuleiro
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
